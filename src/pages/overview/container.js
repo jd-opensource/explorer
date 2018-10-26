499a41137c152e150334dfe11a914e64ea10ab09
jinlong12 <jinlong@jd.com>
2018-10-26 10:06:04 +0800

    con

--- src/pages/overview/container.js
+++ src/pages/overview/container.js
@@ -105,7 +105,7 @@ class DataTable extends Component {
               <span className={`${styles.val}`}>
                 {store.userTable && 
                   store.userTable[`${num}`] && 
-                    `${store.userTable[`${num}`]['consensusAddress']['host']}:${store.userTable[`${num}`]['consensus']['port']}` 
+                    `${store.userTable[`${num}`]['consensusAddress']['host']}:${store.userTable[`${num}`]['consensusAddress']['port']}` 
                       || ''
                 }
               </span>
