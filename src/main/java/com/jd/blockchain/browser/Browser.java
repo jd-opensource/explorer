36f72a273b53a38d9118f83b263c14d10756aa4e
shaozhuguang <shaozhuguang@jd.com>
2018-11-20 09:19:02 +0800

    版本号修改为0.5.4

---
+++ src/main/java/com/jd/blockchain/browser/Browser.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright: Copyright 2016-2020 JD.COM All Right Reserved
+ * FileName: com.jd.blockchain.browser.Browser
+ * Author: shaozhuguang
+ * Department: Y事业部
+ * Date: 2018/10/29 下午2:45
+ * Description:
+ */
+package com.jd.blockchain.browser;
+
+/**
+ *
+ * @author shaozhuguang
+ * @create 2018/10/29
+ * @since 1.0.0
+ */
+
+public class Browser {
+    public static void main(String[] args) {
+        System.out.println("Version=0.5.0.RELEASE");
+    }
+}
